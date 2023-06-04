b994479ad76e37981d67260f5db4c3a784d775b8
Aaryan Giri <aaryangir@gmail.com>
2023-06-04 14:29:42 +0530

    dictionary

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 url = "https://www.siliconvalleytemple.net/event_calendar.jsp"
 
 r = requests.get(url)
-
 htmlContent = r.content
 
 soup = BeautifulSoup(htmlContent, 'html.parser')
-# print(soup.prettify)
-
 tables = soup.select('div.contents table')
 
 information_list = []
@@ -22,8 +20,52 @@ for table in tables:
             date = columns[0].get_text(strip=True)
             event = columns[1].get_text(strip=True)
 
-            information_list.append((date, event))
+            if event and event[0].isalpha():
+                match = re.search(r'\d', event)
+                if match:
+                    index = match.start()
+                    new_string = event[index:]
+                    information_list.append((date, new_string))
+            else:
+                information_list.append((date, event))
 
-for item in information_list:
-    print(item)
+# for i in information_list:
+#     print(i)
 
+information_dict = []
+
+for date, event in information_list:
+    indices1 = []
+    indices2 = []
+
+    for i in range(len(event)):
+        if event[i] == 'M':
+            indices1.append(i)
+        if event[i] == ":":
+            indices2.append(i)
+
+    for i in indices1:
+        for j in indices2:
+            if j - i == 1:
+                index = j
+
+    time = event[:index]
+    event_name = event[index+2:]
+
+    for i in range(len(time)):
+        if time[i] == "t":
+            ind = i
+
+    start_time = time[:ind-1]
+    end_time = time[ind+3:]
+
+    event_dict = {
+        'Date': date,
+        'Start Time': start_time,
+        'End Time': end_time,
+        'Event Name': event_name
+    }
+    information_dict.append(event_dict)
+
+for event in information_dict:
+    print(event)
